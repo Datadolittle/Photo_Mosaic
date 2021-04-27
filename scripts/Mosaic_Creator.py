fef094b4071927cadcddd1e991f692c449fd485b
Eskapp <elise.epaillard@gmail.com>
2021-04-26 21:43:54 -0400

    Improve code syntax

--- scripts/Mosaic_Creator.py
+++ scripts/Mosaic_Creator.py
@@ -1,51 +1,39 @@
-#!/usr/bin/env python
-import os, random, argparse
-from PIL import Image
+import os, random
+
 import numpy as np
-
-parser = argparse.ArgumentParser(description='Creates a photomosaic from input images')
-parser.add_argument('--target', dest='target', required=True, help="Image to create mosaic from")
-parser.add_argument('--images', dest='images', required=True, help="Diectory of images")
-parser.add_argument('--grid', nargs=2, dest='grid', required=True, help="Size of photo mosaic")
-parser.add_argument('--output', dest='output', required=False)
-
-args = parser.parse_args()
+from PIL import Image
 
 
-def getImages(images_directory):
-    files = os.listdir(images_directory)
+def get_images(images_directory):
     images = []
-    for file in files:
-        filePath = os.path.abspath(os.path.join(images_directory, file))
-        try:
-            fp = open(filePath, "rb")
-            im = Image.open(fp)
-            images.append(im)
+    for file_name in os.listdir(images_directory):
+        file_path = os.path.join(images_directory, file_name)
+        with open(file_path, 'rb') as f:
+            im = Image.open(f)
             im.load()
-            fp.close()
-        except:
-            print("Invalid image: %s" % (filePath,))
-    return (images)
+        images.append(im)
+
+    return images
 
 
-def getAverageRGB(image):
+def get_average_rgb_value(image):
     im = np.array(image)
     w, h, d = im.shape
-    return (tuple(np.average(im.reshape(w * h, d), axis=0)))
+    return tuple(np.average(im.reshape(w * h, d), axis=0))
 
 
-def splitImage(image, size):
-    W, H = image.size[0], image.size[1]
+def split_image(image, size):
+    original_width, original_height = image.size[0], image.size[1]
     m, n = size
-    w, h = int(W / n), int(H / m)
+    w, h = int(original_width / n), int(original_height / m)
     imgs = []
     for j in range(m):
         for i in range(n):
             imgs.append(image.crop((i * w, j * h, (i + 1) * w, (j + 1) * h)))
-    return (imgs)
+    return imgs
 
 
-def getBestMatchIndex(input_avg, avgs):
+def get_best_match_index(input_avg, avgs):
     avg = input_avg
     index = 0
     min_index = 0
@@ -58,10 +46,10 @@ def getBestMatchIndex(input_avg, avgs):
             min_dist = dist
             min_index = index
         index += 1
-    return (min_index)
+    return min_index
 
 
-def createImageGrid(images, dims):
+def create_image_grid(images, dims):
     m, n = dims
     width = max([img.size[0] for img in images])
     height = max([img.size[1] for img in images])
@@ -70,93 +58,77 @@ def createImageGrid(images, dims):
         row = int(index / n)
         col = index - n * row
         grid_img.paste(images[index], (col * width, row * height))
-    return (grid_img)
+    return grid_img
 
 
-def createPhotomosaic(target_image, input_images, grid_size,
-                      reuse_images=True):
-    target_images = splitImage(target_image, grid_size)
+def createPhotomosaic(target_image, input_images, grid_size, reuse_images=True):
+    target_patches = split_image(target_image, grid_size)
 
     output_images = []
     count = 0
-    batch_size = int(len(target_images) / 10)
-    avgs = []
-    for img in input_images:
-        try:
-            avgs.append(getAverageRGB(img))
-        except ValueError:
-            continue
+    batch_size = int(len(target_patches) / 10)
 
-    for img in target_images:
-        avg = getAverageRGB(img)
-        match_index = getBestMatchIndex(avg, avgs)
+    candidate_imgs_avg_rgb_values = [get_average_rgb_value(img) for img in input_images]
+
+    for patch in target_patches:
+        target_patch_abg_rgb = get_average_rgb_value(patch)
+        match_index = get_best_match_index(target_patch_abg_rgb, candidate_imgs_avg_rgb_values)
         output_images.append(input_images[match_index])
         if count > 0 and batch_size > 10 and count % batch_size is 0:
-            print('processed %d of %d...' % (count, len(target_images)))
+            print(f'processed {count} of {len(target_patches)}...')
         count += 1
         # remove selected image from input if flag set
         if not reuse_images:
             input_images.remove(match_index)
 
-    mosaic_image = createImageGrid(output_images, grid_size)
-    return (mosaic_image)
+    mosaic_image = create_image_grid(output_images, grid_size)
+    return mosaic_image
 
 
-### ---------------------------------------------
+def main(target_image, patch_images_dir, save_path, patch_size=50, reuse_images=True):
+    target_image = Image.open(target_image)
+    # Get image size
+
+    # Adjust size to fit closest number of patches
 
 
-target_image = Image.open(args.target)
-
-# input images
-print('reading input folder...')
-input_images = getImages(args.images)
-
-# check if any valid input images found
-if input_images == []:
-    print('No input images found in %s. Exiting.' % (args.images,))
-    exit()
-
-# shuffle list - to get a more varied output?
-random.shuffle(input_images)
-
-# size of grid
-grid_size = (int(args.grid[0]), int(args.grid[1]))
-
-# output
-output_filename = 'mosaic.jpeg'
-if args.output:
-    output_filename = args.output
-
-# re-use any image in input
-reuse_images = True
-
-# resize the input to fit original image size?
-resize_input = True
-
-print('starting photomosaic creation...')
-
-# if images can't be reused, ensure m*n <= num_of_images
-if not reuse_images:
-    if grid_size[0] * grid_size[1] > len(input_images):
-        print('grid size less than number of images')
-        exit()
-
-# resizing input
-if resize_input:
+    patch_images = get_images(patch_images_dir)
     print('resizing images...')
-    # for given grid size, compute max dims w,h of tiles
-    dims = (int(target_image.size[0] / grid_size[1]),
-            int(target_image.size[1] / grid_size[0]))
-    print("max tile dims: %s" % (dims,))
-    # resize
-    for img in input_images:
-        img.thumbnail(dims)
+    for img in patch_images:
+        img.thumbnail((patch_size, patch_size))
 
-# create photomosaic
-mosaic_image = createPhotomosaic(target_image, input_images, grid_size, reuse_images)
+    # check if any valid input images found
+    if patch_images == []:
+        raise ValueError(f'No input images found in {patch_images_dir}. Exiting.')
 
-# write out mosaic
-mosaic_image.save(output_filename, 'jpeg')
+    # shuffle list - to get a more varied output?
+    random.shuffle(patch_images)
 
-print("saved output to %s" % (output_filename,))
-print('done.')
+    # size of grid
+    grid_size = (patch_size, patch_size)
+
+    # output
+    output_filename = 'mosaic_elise.jpeg'
+    if save_path:
+        output_filename = save_path
+
+    print('starting photomosaic creation...')
+    # if images can't be reused, ensure m*n <= num_of_images
+    if not reuse_images:  # TODO - Change condition
+        if grid_size[0] * grid_size[1] > len(patch_images):
+            raise ValueError('grid size less than number of images')
+
+    # create photomosaic
+    mosaic_image = createPhotomosaic(target_image, patch_images, grid_size, reuse_images)
+
+    # write out mosaic
+    mosaic_image.save(output_filename, 'jpeg')
+
+    print(f'saved output to {output_filename}')
+
+
+
+if __name__ == '__main__':
+    main(os.path.join(os.path.dirname(__file__), '..', 'data', 'Tree.jpg'),
+         os.path.join(os.path.dirname(__file__), '..', 'data', 'Trees'),
+         os.path.join(os.path.dirname(__file__), '..', 'data'))
